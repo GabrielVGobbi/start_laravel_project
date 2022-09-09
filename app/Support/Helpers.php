19d706aa9ee859732266bcec4d91d3d54dcfd95c
gabrielgobbi <icloud.gobbi@gmail.com>
2022-09-09 12:36:24 -0300

    Adição das funções Helpers Globais

---
+++ app/Support/Helpers.php
@@ -0,0 +1,31 @@
+<?php
+
+use Carbon\Carbon;
+use Carbon\CarbonInterface;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * Makes translation fall back to specified value if definition does not exist
+ *
+ * @param string $key
+ * @param null|string $fallback
+ * @param null|string $locale
+ * @param array|null $replace
+ *
+ * @return array|\Illuminate\Contracts\Translation\Translator|null|string
+ */
+function __trans($key = '', ?string $fallback = null, ?string $locale = null, ?array $replace = [])
+{
+    if (\Illuminate\Support\Facades\Lang::has($key, $locale) && !empty($key)) {
+        return trans($key, $replace, $locale);
+    }
+    return $key;
+}
+
+function _mix($path)
+{
+    return config('app.env')  == 'production' ? url(asset(mix($path))) : asset($path);
+}
